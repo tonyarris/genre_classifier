3eee5ce4b5dc49d4b64dc1ac1b284a36d1f3a8e8
tonyarris <ash552@york.ac.uk>
2021-03-28 18:32:56 +0200

    added index.html

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask import render_template
 from main import *
 from urllib import parse as prs
 
 app = Flask(__name__)
 api = Api(app)
 
+# render homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# TODO take input from form, extract final parameter and pass to a redirect
+
+# predict genre based on link parameter
 class predict(Resource):
     def get(self, link):
         link = prs.quote(link)
@@ -13,6 +22,7 @@ class predict(Resource):
         return {"data": result}
 
 api.add_resource(predict, "/predict/<string:link>")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
